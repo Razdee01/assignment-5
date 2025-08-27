f04f656850a9e8f2d63d42d2d4316fbfb6f9bbc7
Razdee01 <rahmanrajdee@gmail.com>
2025-08-28 03:45:24 +0600

    callBtn-FullyFunctional

--- js/index.js
+++ js/index.js
@@ -1,57 +1,61 @@
-
 function getElement(id) {
   return document.getElementById(id);
 }
 const heart = getElement("heart");
 const emptyHearts = document.getElementsByClassName("empty-heart");
-for(let emptyHeart of emptyHearts) {
-  emptyHeart.addEventListener("click", function() {
+for (let emptyHeart of emptyHearts) {
+  emptyHeart.addEventListener("click", function () {
     let currentHeart = Number(heart.innerText);
-    currentHeart= currentHeart + 1;
+    currentHeart = currentHeart + 1;
     heart.innerText = currentHeart;
   });
 }
 // call buttons
 const callBtns = document.getElementsByClassName("call-btns");
-for(let callBtn of callBtns) {
-  callBtn.addEventListener("click", function() {
-    const coins1 = getElement("coin-id")
+for (let callBtn of callBtns) {
+  callBtn.addEventListener("click", function () {
+    // coins1
+    const coins1 = getElement("coin-id");
     let totalCoin = Number(coins1.innerText);
     if (totalCoin <= 0) {
-      alert("you dont have enogh coins");
+      alert(
+        "You dont have enough coins. To make a call you atleast need 20 coins."
+      );
       return;
     }
-    const subHeadBtn=callBtn.parentNode.parentNode.children[1].children[1].innerText
-    const phoneBtn=callBtn.parentNode.parentNode.children[2].children[0].innerText
+    const subHeadBtn =
+      callBtn.parentNode.parentNode.children[1].children[1].innerText;
+    const phoneBtn =
+      callBtn.parentNode.parentNode.children[2].children[0].innerText;
     alert("📞 Calling " + subHeadBtn + " " + phoneBtn + "...");
-    
-    const historyContainer=getElement("history-container");
-    const newDiv = document.createElement("div")
+    const heading =
+      callBtn.parentNode.parentNode.children[1].children[0].innerText;
+
+    const historyContainer = getElement("history-container");
+    const newDiv = document.createElement("div");
+    const now = new Date();
+    const local = now.toLocaleTimeString();
     newDiv.innerHTML = `
     <div class=" bg-[#d4d1d1] md:flex justify-between items-center p-4 rounded-lg mb-3">
     <div >
-      <h2 class="font-semibold">${subHeadBtn}</h2>
+      <h2 class="font-semibold">${heading}</h2>
       <p>${phoneBtn}</p>
     </div>
     <div>
-      <p>11:36:58 AM</p>
+      <p>${local}</p>
     </div>
     
 
   </div>`;
-    historyContainer.append(newDiv)
-    // coins function
+    historyContainer.append(newDiv);
+    // coin2 function
     const coins = getElement("coin-id");
     let currentcoin = Number(coins.innerText);
-    currentcoin = currentcoin - 50;
+    currentcoin = currentcoin - 20;
     coins.innerText = currentcoin;
 
-   
     getElement("clear-btn").addEventListener("click", function () {
-      newDiv.innerHTML=""
-     
+      newDiv.innerHTML = "";
     });
-
   });
 }
-
